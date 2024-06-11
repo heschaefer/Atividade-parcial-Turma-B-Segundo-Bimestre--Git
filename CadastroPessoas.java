ba8848cb2e585005e6c266d1eef7cdc8d471edc4
Henryque <henryque.schaefer@outlook.com>
2024-06-10 21:02:01 -0300

    Retorna lista das pessoas

---
+++ CadastroPessoas.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class CadastroPessoas {
+    private static ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+
+    public static ArrayList<Pessoa> getListaPessoas() {
+        return listaPessoas;
+    }
+}
